a6b4f072be908f910902dfdbe48795fb3ebb443a
Om <omk27572.0@gmail.com>
2023-08-24 16:18:12 +0530

    cleaned

--- search_google.py
+++ search_google.py
@@ -16,27 +16,4 @@ def google_search(command):
     subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe")
     time.sleep(2)
     pyautogui.hotkey('ctrl', 'v')
-    # pyautogui.write("")  # Write the URL
     pyautogui.press('enter')
-
-
-
-    # command = command.replace("google", "").strip()
-    # speak(f"searching: {command}")
-    # # command=command+".com"
-    # # commnad = urllib.parse.quote(command, safe=" :/?&=")
-    # # print(command)
-    # # subprocess.Popen(["C:\Program Files\Google\Chrome\Application\chrome.exe",command])
-    # # browser_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-    # pyautogui.press("win")  # Open the Start menu
-    # pyautogui.write("C:\Program Files\Google\Chrome\Application\chrome.exe")  # Type the browser path
-    # pyautogui.press("enter")  # Press Enter
-
-    # # Wait for the browser to open (adjust duration if needed)
-    # time.sleep(3)
-
-    # # Simulate typing the command in the search bar
-    # pyautogui.hotkey("ctrl", "t")  # Open a new tab
-    # pyautogui.write(command)  # Type the command
-    # pyautogui.press("enter")  # Press Enter
-    # time.sleep(5)
